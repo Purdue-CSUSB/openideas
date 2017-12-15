871098b0e85ae1cbd4b050ec8e7735cd7ee73c50
Matthew Ess <mess@yelp.com>
2017-12-15 11:16:35 -0500

    Add basic commenting

--- backend/routes/ideas.js
+++ backend/routes/ideas.js
@@ -23,13 +23,9 @@ router.route('/ideas/:idea_id')
       .catch(err => res.send(err));
   })
   .patch((req, res) => {
-    Idea.findById(req.params.idea_id).exec()
-      .then((idea) => {
-        idea.set(req.body);
-        idea.save()
-          .then(() => res.json(idea))
-          .catch(err => res.send(err));
-      });
+    Idea.findByIdAndUpdate(req.params.idea_id, { $set: req.body }, { new: true }).exec()
+      .then(idea => res.json(idea))
+      .catch(err => res.send(err));
   })
   .delete((req, res) => {
     Idea.remove({ _id: req.params.idea_id })
@@ -39,13 +35,20 @@ router.route('/ideas/:idea_id')
 
 router.route('/idea/:idea_id/vote')
   .post((req, res) => {
-    Idea.findById(req.params.idea_id).exec()
-      .then((idea) => {
-        idea.set({ votes: idea.votes + 1 });
-        idea.save()
-          .then(() => res.json(idea))
-          .catch(err => res.send(err));
-      });
+    Idea.findByIdAndUpdate(req.params.idea_id, { $inc: { votes: 1 } }, { new: true }).exec()
+      .then(idea => res.json(idea))
+      .catch(err => res.send(err));
+  });
+
+router.route('/idea/:idea_id/comment')
+  .post((req, res) => {
+    Idea.findByIdAndUpdate(
+      req.params.idea_id,
+      { $push: { comments: { author: req.body.author, body: req.body.body } } },
+      { new: true },
+    ).exec()
+      .then(idea => res.json(idea))
+      .catch(err => res.send(err));
   });
 
 module.exports = router;
