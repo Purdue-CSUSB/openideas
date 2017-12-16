71d0316f0e9fdcdcd6af7b873bdb2fc362467a83
Matthew Ess <mess@yelp.com>
2017-12-16 14:14:10 -0500

    Ugly skeleton

---
+++ frontend/components/ideaCard.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+#idea-card
+  .card.bg-secondary
+    .columns
+      .col-mr-auto.text-center#votes
+        button.btn.btn-sm.btn-primary
+          i.icon.icon-arrow-up
+        h3.card-title {{ idea.votes }}
+      .column
+        .card-header
+          router-link.h5.card-title(:to='"/" + idea._id') {{ idea.title }}
+        .card-body
+          p {{ idea.description }}
+</template>
+
+<script>
+export default {
+  name: 'idea-card',
+  props: {
+    idea: Object,
+    index: Number,
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.card
+  margin: 15px
+#votes
+  margin: 30px
+</style>
