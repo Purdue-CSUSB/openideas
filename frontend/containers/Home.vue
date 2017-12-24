df80f509cb894de63089e294ed7aafb7bd9f581e
Jay Hankins <hankins.jay@gmail.com>
2017-12-24 17:42:19 -0500

    Lint and test

--- frontend/containers/Home.vue
+++ frontend/containers/Home.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
 #home
+  <p>{{ ideas[0].title }}</p>
 </template>
 
 <script>
@@ -7,9 +8,20 @@ export default {
   name: 'Home',
   data() {
     return {
+      message: 'Hello Vue.js!',
       ideas: [
-        { title: 'Make CS better', description: 'Fix stuff', author: 'mess@purdue.edu', votes: 1 },
-        { title: 'Make CS worse', description: 'Break stuff', author: 'jh@purdue.edu', votes: 0 },
+        {
+          title: 'Make CS better',
+          description: 'Fix stuff',
+          author: 'mess@purdue.edu',
+          votes: 1,
+        },
+        {
+          title: 'Make CS worse',
+          description: 'Break stuff',
+          author: 'jh@purdue.edu',
+          votes: 0,
+        },
       ],
     };
   },
