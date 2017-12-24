41026b5e7856da2d23db5b4ea30976f9be8d7837
Jay Hankins <hankins.jay@gmail.com>
2017-12-24 17:27:58 -0500

    Fix component rename

--- frontend/router/index.js
+++ frontend/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Login from '@/containers/Login';
+import Home from '@/containers/Home';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Login',
-      component: Login,
+      name: 'Home',
+      component: Home,
     },
   ],
 });
