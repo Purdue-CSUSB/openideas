244bd13b95a6f3271227a815042ebb02a18d016f
Jay Hankins <hankins.jay@gmail.com>
2018-01-09 10:30:51 -0500

    important changes

--- frontend/components/hero.vue
+++ frontend/components/hero.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
 #hero
+  p Hey ya fucks!
 </template>
 
 <script>
--- frontend/components/siteHeader.vue
+++ frontend/components/siteHeader.vue
@@ -2,12 +2,16 @@
 #site-header
   header.navbar
     section.navbar-section
-      a.navbar-brand.mr-2(href='#') OpenIdeas
-      a.btn.btn-link.active(href='#') Home
-      a.btn.btn-link(href='#') All Ideas
-      a.btn.btn-link(href='#') About
+      a.navbar-brand.mr-2(href='#')
+        span OpenIdeas
+      a.btn.btn-link.active(href='#')
+        span Home
+      a.btn.btn-link(href='#')
+        span All Ideas
+      a.btn.btn-link(href='#')
+        span About
     section.navbar-section
-      p user botton?
+      a user botton?
 </template>
 
 <script>
