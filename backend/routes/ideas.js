bca977558132d1f6af50ef57641a194c64c3991f
Matthew Ess <mess@yelp.com>
2018-01-17 16:11:55 -0500

    Barebones passport

--- backend/routes/ideas.js
+++ backend/routes/ideas.js
@@ -22,17 +22,6 @@ router.route('/ideas/:idea_id')
       .then(idea => res.json(idea))
       .catch(err => res.send(err));
   });
-  // TODO: these are probably a security flaw
-  // .patch((req, res) => {
-  //   Idea.findByIdAndUpdate(req.params.idea_id, { $set: req.body }, { new: true }).exec()
-  //     .then(idea => res.json(idea))
-  //     .catch(err => res.send(err));
-  // })
-  // .delete((req, res) => {
-  //   Idea.remove({ _id: req.params.idea_id })
-  //     .then(() => res.json({ success: true }))
-  //     .catch(err => res.send(err));
-  // });
 
 router.route('/idea/:idea_id/vote')
   .post((req, res) => {
