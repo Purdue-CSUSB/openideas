65fe20e42aa9ed849935499440b5542b2640b687
Jay Hankins <hankins.jay@gmail.com>
2018-01-24 12:50:01 -0500

    App styles

--- frontend/App.vue
+++ frontend/App.vue
@@ -11,5 +11,12 @@ export default {
 
 <style lang="scss">
 @import 'assets/openideas.scss';
+#app:first-child {
+  padding-top: 0.85rem;
+}
+#app {
+  border-top-style: solid;
+  border-top-width: 0.3rem;
+  border-top-color: $primary-color;
+}
 </style>
-``
