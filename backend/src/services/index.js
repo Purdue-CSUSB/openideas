1306988211698b53cdc8bf1f1326ad2d4c094852
Matthew Ess <mess@yelp.com>
2018-02-12 14:41:03 -0500

    ES6ify, try 2

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -2,7 +2,7 @@ import users from './users/users.service';
 import ideas from './ideas/ideas.service';
 
 // eslint-disable-next-line no-unused-vars
-module.exports = (app) => {
+export default (app) => {
   app.configure(users)
     .configure(ideas);
 };
