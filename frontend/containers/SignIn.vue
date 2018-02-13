2b8b09ebc873e34422d260d7ed24f6eba1dedfaf
Jay Hankins <hankins.jay@gmail.com>
2018-02-13 12:05:41 -0500

    signnin update

--- frontend/containers/SignIn.vue
+++ frontend/containers/SignIn.vue
@@ -1,21 +1,23 @@
 <template lang="pug">
-#signin.container.grid-lg
-  <div class="modal modal-lg">
-    <a href="#close" class="modal-overlay" aria-label="Close"></a>
-    <div class="modal-container">
-      div.loading.loading-lg
+<transition name="fade" mode="in-out">
+  #signin.container.grid-lg
+    <div class="modal modal-lg">
+      <a href="#close" class="modal-overlay" aria-label="Close"></a>
+      <div class="modal-container">
+        div.loading.loading-lg
+      </div>
     </div>
-  </div>
-  div.columns
-    div.column.col-4.col-mx-auto
-      h3.text-center Welcome
-      form
-        div.form-group
-          label.form-label(for="email") Email
-          input.form-input(type="text", id="email", placeholder="pete@purdue.edu")
+    div.columns
+      div.column.col-4.col-mx-auto
+        h3.text-center Welcome
+        form
+          div.form-group
+            label.form-label(for="email") Email
+            input.form-input(type="text", id="email", placeholder="pete@purdue.edu")
 
-        button.btn.btn-primary.btn-block Next
-      p.text-center Need an account? #[button.btn.btn-link Sign Up]
+          button.btn.btn-primary.btn-block Next
+        p.text-center Need an account? #[button.btn.btn-link Sign Up]
+</transition>
 
 </template>
 
@@ -38,5 +40,12 @@ p, .btn-link {
 
 button {
   padding-top: 0.2rem;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
