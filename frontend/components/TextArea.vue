64dfd26eaf9b1671db5480ae538535845b5e504e
Jay Hankins <hankins.jay@gmail.com>
2018-02-23 09:02:52 -0500

    Alter JWT handling for viewing; adds textarea component and uses it in New.vue

---
+++ frontend/components/TextArea.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.form-group
+  label.form-label(v-if='label') {{ label }}
+  textarea.form-input(
+    :placeholder='placeholder'
+    @input='event => $emit("input", event.target.value)'
+  )
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    placeholder: String,
+  },
+};
+</script>
--- frontend/containers/New.vue
+++ frontend/containers/New.vue
@@ -3,7 +3,7 @@
   h3 New Idea
   .form
     TextField(v-model='title' label='Title' placeholder='My Awesome Idea!')
-    TextField(v-model='description' label='Description' placeholder='Tell everyone about your idea- this is where the discussion begins!')
+    MyTextArea(v-model='description' label='Description' placeholder='Tell everyone about your idea- this is where the discussion begins!')
     .form-group
       custom-button(type="primary" :onClick='submitIdea({ title, description, author: user.name })') Submit
 </template>
@@ -12,12 +12,14 @@
 import { mapActions, mapState } from 'vuex';
 import CustomButton from '@/components/CustomButton';
 import TextField from '@/components/TextField';
+import MyTextArea from '@/components/TextArea';
 
 export default {
   name: 'New',
   components: {
     CustomButton,
     TextField,
+    MyTextArea,
   },
   data() {
     return {
